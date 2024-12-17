539375e855b6111097b1812a028ab1197f0f0aaa
tatitati <albertf@ryanair.com>
2024-12-17 19:42:31 +0100

    added fuzzy mode

--- graba/main.py
+++ graba/main.py
@@ -3,113 +3,141 @@ import string
 from datetime import date, datetime, timedelta
 from dateutil.parser import parse
 
-class Any:
+class Any():
 
-    @staticmethod
-    def of(options: list):
+    def __init__(self, fuzzy_mode: bool=False):
+        self._fuzzy_mode = fuzzy_mode
+
+    def of(self, options: list):
         return random.choice(options)
 
-    @staticmethod
-    def listOf(min, max, factoryFunction):
+
+    def listOf(self, min, max, factoryFunction):
         result = []
-        amount = Any.positiveNumber(min, max)
+        amount = self.positiveNumber(min, max)
         for i in range(0, amount):
             data = factoryFunction()
             result.append(data)
 
         return result
 
-    @staticmethod
-    def subsetOf(min, max, items):
+    def subsetOf(self, min, max, items):
         items_randomized = random.sample(items, len(items))
         if min == None:
             min=1
         if max == None or max > len(items):
             max=len(items)-1
 
-        amount_to_get = Any.positiveNumber(min=min, max=max)
+        amount_to_get = self.positiveNumber(min=min, max=max)
         return items_randomized[:amount_to_get]
 
-    @staticmethod
-    def positiveNumber(min: int = 0, max=9999) -> int:
-        return random.randrange(min, max)
+    def positiveNumber(self, min: int = 0, max=9999) -> int:
+        number=random.randrange(min, max)
+        if self._fuzzy_mode == True:
+            return self.of([int(number), str(number), float(number)])
 
-    @staticmethod
-    def negativeNumber(min: int = -9999, max=0) -> int:
-        return random.randrange(min, max)
+        return number
 
-    @staticmethod
-    def anyNumber() -> int:
-        return Any.of([Any.positiveNumber(), Any.negativeNumber()])
+    def negativeNumber(self, min: int = -9999, max=0) -> int:
+        number = random.randrange(min, max)
 
-    @staticmethod
-    def rating() -> int:
-        return Any.positiveNumber(min=0, max=5)
+        if self._fuzzy_mode == True:
+            return self.of([int(number), str(number), float(number)])
 
-    @staticmethod
-    def anyLetter() -> str:
-        return Any.of(list(string.ascii_lowercase))
+        return number
 
-    @staticmethod
-    def word(min = 1, max = 30) -> str:
-        length = Any.positiveNumber(min, max)
+    def anyNumber(self) -> int:
+        return self.of([self.positiveNumber(), self.negativeNumber()])
+
+    def rating(self) -> int:
+        number = self.positiveNumber(min=0, max=5)
+
+        if self._fuzzy_mode == True:
+            return self.of([int(number), str(number), float(number)])
+
+        return number
+
+    def anyLetter(self) -> str:
+        letter = self.of(list(string.ascii_lowercase))
+
+        if self._fuzzy_mode == True:
+            return self.of([f" {letter}", letter, f"{letter} ", f" {letter} "])
+
+        return letter
+
+    def word(self, min = 1, max = 30) -> str:
+        length = self.positiveNumber(min, max)
         alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 
-        return ''.join(random.choice(alphabet) for i in range(length))
+        word = ''.join(random.choice(alphabet) for i in range(length))
 
-    @staticmethod
-    def sentence(min_words: int = 1, max_words: int = 5) -> str:
-        amount_words = Any.positiveNumber(min=min_words, max=max_words)
+        if self._fuzzy_mode == True:
+            return self.of([f" {word}", word, f"{word} ", f" {word} "])
+
+        return word
+
+    def sentence(self, min_words: int = 1, max_words: int = 5) -> str:
+        amount_words = self.positiveNumber(min=min_words, max=max_words)
         words=[]
         for i in range(amount_words):
             words.append(Any.word())
 
-        return ' '.join(words)
+        sentence = ' '.join(words)
 
-    @staticmethod
-    def bytes(min=1, max=30) -> bytes:
-        word = Any.word(min, max)
+        if self._fuzzy_mode == True:
+            return self.of([f" {sentence}", sentence, f"{sentence} ", f" {sentence} "])
+
+        return sentence
+
+    def bytes(self, min=1, max=30) -> bytes:
+        word = self.word(min, max)
         return word.encode('UTF-8')
 
-    @staticmethod
-    def url(min=1, max=4) -> str:
-        length = Any.positiveNumber(min, max)
+
+    def url(self, min=1, max=4) -> str:
+        length = self.positiveNumber(min, max)
         words = []
         for i in range(length):
             words.append(Any.word())
 
         return "http://www/"+'.'.join(words)
 
-    @staticmethod
-    def bool() -> bool:
-        return Any.of([True, False])
+    def bool(self) -> bool:
+        boolean = self.of([True, False])
+        if self._fuzzy_mode == True:
+            return self.of([boolean, "true", "false", "True", "False", 1, 0])
 
-    @staticmethod
-    def dateTimeInPast() -> datetime:
+        return boolean
+
+    def null(self):
+        if self._fuzzy_mode == True:
+            return self.of([None, "None", "none", "null"])
+
+        return None
+
+
+    def dateTimeInPast(self) -> datetime:
         now = datetime.now()
         return now - timedelta(days = Any.positiveNumber())
 
-    @staticmethod
-    def dateTimeInFuture() -> datetime:
+
+    def dateTimeInFuture(self) -> datetime:
         now = datetime.now()
         return now + timedelta(days=Any.positiveNumber())
 
-    @staticmethod
-    def dateTimeBefore(beforeDateTime) -> datetime:
+    def dateTimeBefore(self, beforeDateTime) -> datetime:
         if isinstance(beforeDateTime, str):
             beforeDateTime = parse(beforeDateTime, fuzzy=True)
 
         return beforeDateTime - timedelta(days=Any.positiveNumber())
 
-    @staticmethod
-    def dateTimeAfter(afterDateTime) -> datetime:
+    def dateTimeAfter(self, afterDateTime) -> datetime:
         if isinstance(afterDateTime, str):
             afterDateTime = parse(afterDateTime, fuzzy=True)
 
         return afterDateTime + timedelta(days=Any.positiveNumber())
 
-    @staticmethod
-    def datetimeBetween(from_datetime, to_datetime) -> datetime:
+    def datetimeBetween(self, from_datetime, to_datetime) -> datetime:
         if isinstance(from_datetime, str):
             from_datetime = parse(from_datetime, fuzzy=True)
 
@@ -119,20 +147,17 @@ class Any:
         from_datetime_ts = from_datetime.timestamp()
         to_datetime_ts = to_datetime.timestamp()
 
-        random_ts_between = Any.positiveNumber(min=from_datetime_ts, max=to_datetime_ts)
+        random_ts_between = self.positiveNumber(min=from_datetime_ts, max=to_datetime_ts)
 
         return datetime.fromtimestamp(random_ts_between)
 
-    @staticmethod
-    def dateTime() -> datetime:
-        return Any.of([Any.dateTimeInPast(), Any.dateTimeInFuture()])
+    def dateTime(self) -> datetime:
+        return self.of([self.dateTimeInPast(), self.dateTimeInFuture()])
 
-    @staticmethod
-    def date() -> date:
-        return Any.dateTime().date()
+    def date(self) -> date:
+        return self.dateTime().date()
 
-    @staticmethod
-    def latitude(min: int = None, max: int = None) -> str:
+    def latitude(self, min: int = None, max: int = None) -> str:
         min=-90 if min == None else min
         max=90  if max == None else max
         decimal_places = 4
@@ -140,8 +165,7 @@ class Any:
 
         return f"{round(random_lat, decimal_places)}"
 
-    @staticmethod
-    def longitude(min: int = None, max: int = None) -> str:
+    def longitude(self, min: int = None, max: int = None) -> str:
         min = -180 if min == None else min
         max =  180 if max == None else max
         decimal_places = 4
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,15 @@ from main import Any
 
 
 def test_positiveNumber():
+    any = Any()
     def createData():
         return {
-            "age": Any.positiveNumber(),
-            "name": Any.word()
+            "age": any.positiveNumber(),
+            "name": any.word()
         }
 
 
-    result = Any.listOf(
+    result = any.listOf(
         min=3,
         max=7,
         factoryFunction=createData
@@ -26,19 +27,19 @@ def test_positiveNumber():
 
 
 def test_subsetOf():
-    result = Any.subsetOf(min=1, max=4, items=["a", "b", "c", "d", "e", "f"])
+    result = Any().subsetOf(min=1, max=4, items=["a", "b", "c", "d", "e", "f"])
     print(result) # ['d', 'e', 'b']
 
 def test_dateTimeBefore():
-    result = Any.dateTimeBefore(datetime(2015, 12, 25, 10, 30, 45))
+    result = Any().dateTimeBefore(datetime(2015, 12, 25, 10, 30, 45))
     print(result) # 1994-10-22 10:30:45
 
-    result = Any.dateTimeBefore("2022-10-10")
+    result = Any().dateTimeBefore("2022-10-10")
     print(result) # 2000-03-16 00:00:00
 
-    result = Any.dateTimeBefore("2022-10-10 23:11:05")
+    result = Any().dateTimeBefore("2022-10-10 23:11:05")
     print(result) # 2016-12-28 23:11:05
 
 def test_datetimebetween():
-    result = Any.datetimeBetween("2023-10-10", "2027-09-09")
+    result = Any().datetimeBetween("2023-10-10", "2027-09-09")
     print(result) # 2025-08-10 22:00:19
